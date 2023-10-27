fdee3cf079d683bcac46f88cde2881b0b7d7471d
arta-eth <arta@verticalai.io>
2023-10-27 16:04:36 -0700

    g

--- src/app/asset-view-module/asset-view-module.component.ts
+++ src/app/asset-view-module/asset-view-module.component.ts
@@ -16,7 +16,6 @@ import { World } from '../models/workflow/world.model';
 import { SceneAsset } from '../models/workflow/scene-asset.model';
 import { DesignService } from '../design.service';
 import { ArcRotateCamera } from 'babylonjs';
-import "babylonjs-viewer";
 
 @AutoUnsubscribe
 @Component({
